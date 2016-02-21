0a8399f5e76e1d4e005c3ff5f6ff846e34cfcedb
Marek Strejczek <marek.strejczek@gmail.com>
2016-02-21 23:24:45 +0100

    Added a bit of logging

--- calculator/src/main/java/wdsr/exercise1/Main.java
+++ calculator/src/main/java/wdsr/exercise1/Main.java
@@ -1,10 +1,12 @@
 package wdsr.exercise1;
 
+import java.util.Arrays;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import wdsr.exercise1.logic.Calculator;
 import wdsr.exercise1.conversions.ArrayConverter;
+import wdsr.exercise1.logic.Calculator;
 
 /**
  * Created by Marek on 14.02.2016.
@@ -13,6 +15,7 @@ public class Main {
 	private static final Logger log = LogManager.getLogger();
 	
     public static void main(String[] args) {
+    	log.debug("main entered [args={}]", Arrays.toString(args));
         if (args.length == 0) {
             log.info("No input provided");
         } else {
@@ -21,5 +24,6 @@ public class Main {
 	        log.info("Min="+calculator.min(nums));
 	        log.info("Max="+calculator.max(nums));
         }
+    	log.debug("main exiting");
     }
 }
